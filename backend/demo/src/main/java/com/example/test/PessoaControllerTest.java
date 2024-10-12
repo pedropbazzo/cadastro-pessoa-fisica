65f0f9d4efec0b81ac335164730c980b8fc78257
Pedro Bazzo <pedro.bazzo.ext@epag.com>
2024-10-12 12:11:25 -0300

    test upgrade refatoring

--- backend/demo/src/main/java/com/example/test/PessoaControllerTest.java
+++ backend/demo/src/main/java/com/example/test/PessoaControllerTest.java
@@ -9,89 +9,92 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.sql.Date;
 import java.time.LocalDate;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class PessoaControllerTest extends AbstractTest {
+
     @Override
     @Before
     public void setUp() {
         super.setUp();
     }
+
     @Test
     public void getAllPessoas() throws Exception {
         String uri = "/pessoas";
         MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
-                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+            .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
 
         int status = mvcResult.getResponse().getStatus();
         assertEquals(200, status);
         String content = mvcResult.getResponse().getContentAsString();
-        Pessoa[] pessoaslist = super.mapFromJson(content, Pessoa[].class);
-        assertTrue(pessoaslist.length > 0);
+        Pessoa[] pessoasList = super.mapFromJson(content, Pessoa[].class);
+        assertTrue(pessoasList.length > 0);
     }
 
     @Test
     public void getPessoasByName() throws Exception {
         String name = "Leonardo";
-        String uri = "/pessoas/findByName/"+name;
+        String uri = "/pessoas/findByName/" + name;
         MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
-                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+            .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
 
         int status = mvcResult.getResponse().getStatus();
         assertEquals(200, status);
         String content = mvcResult.getResponse().getContentAsString();
-        Pessoa[] pessoaslist = super.mapFromJson(content, Pessoa[].class);
-        assertTrue(pessoaslist.length > 0);
+        Pessoa[] pessoasList = super.mapFromJson(content, Pessoa[].class);
+        assertTrue(pessoasList.length > 0);
     }
 
+    @Test
     public void createPessoa() throws Exception {
         String uri = "/pessoas";
-        Pessoa p = new Pessoa();
-        p.setId(11);
-        p.setNome("Geovane Ferreira Melo");
-        p.setCpf(Utils.geraCPF());
-        p.setEmail("geovane_ferreira_melo@gmail.com");
-        p.setSexo("M");
-        p.setDataNascimento(LocalDate.now().toString());
-        p.setNaturalidade("Florianopolis");
-        p.setNacionalidade("Brasileiro");
-        String inputJson = super.mapToJson(p);
+        Pessoa pessoa = new Pessoa();
+        pessoa.setId(11);
+        pessoa.setNome("Geovane Ferreira Melo");
+        pessoa.setCpf(Utils.geraCPF());
+        pessoa.setEmail("geovane_ferreira_melo@gmail.com");
+        pessoa.setSexo("M");
+        pessoa.setDataNascimento(LocalDate.now().toString());
+        pessoa.setNaturalidade("Florianopolis");
+        pessoa.setNacionalidade("Brasileiro");
+        String inputJson = super.mapToJson(pessoa);
         MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .content(inputJson)).andReturn();
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .content(inputJson)).andReturn();
 
         int status = mvcResult.getResponse().getStatus();
         assertEquals(201, status);
     }
+
     @Test
-    public void updateProduct() throws Exception {
+    public void updatePessoa() throws Exception {
         String uri = "/pessoas/2";
-        Pessoa p = new Pessoa();
-        p.setNome("Liandra Borges");
-        p.setCpf("345.128.020-53");
-        p.setSexo("F");
-        p.setEmail("liandra_borges@gmail.com");
-        p.setDataNascimento(LocalDate.now().toString());
-        p.setNacionalidade("Brasileira");
-        p.setNaturalidade("Curitiba");
-        String inputJson = super.mapToJson(p);
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Liandra Borges");
+        pessoa.setCpf("345.128.020-53");
+        pessoa.setSexo("F");
+        pessoa.setEmail("liandra_borges@gmail.com");
+        pessoa.setDataNascimento(LocalDate.now().toString());
+        pessoa.setNacionalidade("Brasileira");
+        pessoa.setNaturalidade("Curitiba");
+        String inputJson = super.mapToJson(pessoa);
         MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.put(uri)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .content(inputJson)).andReturn();
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .content(inputJson)).andReturn();
 
         int status = mvcResult.getResponse().getStatus();
         assertEquals(200, status);
     }
+
     @Test
-    public void deleteProduct() throws Exception {
+    public void deletePessoa() throws Exception {
         String uri = "/pessoas/2";
         MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.delete(uri)).andReturn();
         int status = mvcResult.getResponse().getStatus();
         assertEquals(200, status);
     }
-
 }
